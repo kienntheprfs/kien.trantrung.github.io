21a913c2efee4c7d3c90fee937c7bfc70bd32e82
kienntheprfs <kien.trantrung@hcmut.edu.vn>
2024-05-03 12:40:51 +0700

    add login page backend

--- js/main/main.js
+++ js/main/main.js
@@ -1,20 +1,52 @@
 import { getData, setData, updateData, removeData, initFirebase} from './Firebase.js'
-import { uploadFile, downloadFile } from './Firestorage.js'
+import { FirebaseController } from './Firebase.js';
+// import { uploadFile, downloadFile } from './Firestorage.js'
 // import {BasicUserInfo, Student, Instructor} from "./user_info.js";
 class Model {
     constructor(){
         initFirebase();   
     }
-    getStudentData(username) {
-        return new Promise((resolve, reject) => {
-            getData("user/student/" + username)
-            .then((data) => {
-              resolve(data);
-            })
-            .catch((error) => {
-              reject(error);
-            });
+    get(login_type, username) {
+      return new Promise((resolve, reject) => {
+        getData("user/"+login_type+"/" + username)
+        .then((data) => {
+          resolve(data);
+        })
+        .catch((error) => {
+          reject(error);
         });
+    });
+    }
+    setCurrentUser(login_type, username) {
+      return new Promise((resolve, reject) => {
+        setData("current_user/", {login_type: login_type, user_name: username})
+          .then(() => {
+            resolve();
+          })
+          .catch((error) => {
+            reject(error);
+          });
+      });
+    }
+    getCurrentUser() {
+      return new Promise((resolve, reject) => {
+        getData("current_user/")
+        .then((data) => {
+          resolve(data);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+    }
+    getStudentData = (username) => {
+        return this.get("student", username)
+    }
+    getTeacherData = (username) => {
+        return this.get("teacher", username)
+    }
+    getAdminData = (username) => {
+        return this.get("admin", username)
     }
 
     setStudentData(username, student_data) {
@@ -29,33 +61,33 @@ class Model {
         });
       }
       
-      updateStudentData(username, student_data) {
-        // return new Promise((resolve, reject) => {
-          updateData("user/student/" + username + "/personal_info", student_data)
-            // .then(() => {
-            //   resolve("Success");
-            // })
-            // .catch((error) => {
-            //   reject(error);
-            // });
-        // });
-        this.onDataChanged()
-      }
-      
-      removeStudentData(username) {
-        return new Promise((resolve, reject) => {
-          removeData("user/student/" + username)
-            .then(() => {
-              resolve();
-            })
-            .catch((error) => {
-              reject(error);
-            });
-        });
-      }
-      bindOnDataChanged(handler) {
-        this.onDataChanged = handler
-      }
+    updateStudentData(username, student_data) {
+      // return new Promise((resolve, reject) => {
+        updateData("user/student/" + username + "/personal_info", student_data)
+          // .then(() => {
+          //   resolve("Success");
+          // })
+          // .catch((error) => {
+          //   reject(error);
+          // });
+      // });
+      this.onDataChanged()
+    }
+    
+    removeStudentData(username) {
+      return new Promise((resolve, reject) => {
+        removeData("user/student/" + username)
+          .then(() => {
+            resolve();
+          })
+          .catch((error) => {
+            reject(error);
+          });
+      });
+    }
+    bindOnDataChanged(handler) {
+      this.onDataChanged = handler
+    }
     
 }
 
@@ -164,83 +196,121 @@ class View {
       var html = template(context);
       $("#target").html(html);
     }
+    bindLogin(handler) {
+      handler()
+    }
+    bindLogout(handler) {
+      handler()
+    }
 
 }
-
+var user_name = "onLogin"
 class Controller {
     constructor(model, view){
         this.model = model;
         this.view = view;
+        
 
-        this.model.getStudentData("kien_trantrung")
+        this.model.getCurrentUser()
         .then((data) => {
-
-          var path = window.location.pathname;
-          var filename = path.split('/').pop();
-
-          if (filename == 'edit-student.html') {
-            this.view.LoadEditStudentView(data);
-            (function ($) {
-              'use strict';
-            
-              $.mask.definitions['~'] = '[+-]';
-              $('#bhxh-1').mask('9999 9999 99');
-              $('#bhxh-2').mask('9999 / 9999');
-              $('#bhxh-3').mask('B999999999999999-C999');
-              $('#date').mask('99/99/9999');
-              $('#phone').mask('9999 999 999');
-              $('#guardian-phone').mask('9999 999 999');
-              $('#phoneExt').mask('(999) 999-9999? x99999');
-              $('#iphone').mask('+33 999 999 999');
-              $('#tin').mask('99-9999999');
-              $('#ccn').mask('9999 9999 9999 9999');
-              $('#ssn').mask('999-99-9999');
-              $('#currency').mask('999,999,999.99');
-              $('#product').mask('a*-999-a999', {
-                placeholder: ' '
-              });
-              $('#eyescript').mask('~9.99 ~9.99 999');
-              $('#po').mask('PO: aaa-999-***');
-              $('#pct').mask('99%');
-              $('#phoneAutoclearFalse').mask('(999) 999-9999', {
-                autoclear: false
-              });
-              $('#phoneExtAutoclearFalse').mask('(999) 999-9999? x99999', {
-                autoclear: false
-              });
-              $('input').blur(function () {
-                $('#info').html('Unmasked value: ' + $(this).mask());
-              }).dblclick(function () {
-                $(this).unmask();
-              });
-            })(jQuery);
-          }  
-          else if (filename == 'student-details.html') {
-            this.view.loadStudentView(data);
+          console.log(data)
+          if (data.user_name != "onLogin") {  
+            user_name = data.user_name
           }
-          else if (filename == 'student-course.html') {
-            this.view.loadStudentCourseView(data);
+          else if (data.user_name == "") {
+            console.log("No user logged in")
+            var fbcontroller = new FirebaseController()
+            fbcontroller.setData("current_user", {user_name: "onLogin"})
+            window.location.href = "login.html"
           }
-          else if (filename == 'edit-teaching-course.html') {
-            this.view.loadEditTeachingCourse(data);
+          else if (data.user_name == "onLogin") {
+            this.handleLogin()
           }
           
+        })
+        .then(() => {
+          this.model.getStudentData(user_name)
+          .then((data) => {
 
-        })
-        .catch((error) => {
-            console.log(error);
-        })
-        .then(() => {
-          this.view.bindSubmitEditStudent(this.handleEditStudent)
-        })
-        .then(() => {
-          this.model.bindOnDataChanged(this.handleOnDataChanged)
+            var path = window.location.pathname;
+            var filename = path.split('/').pop();
+
+            if (filename == 'edit-student.html') {
+              this.view.LoadEditStudentView(data);
+              (function ($) {
+                'use strict';
+              
+                $.mask.definitions['~'] = '[+-]';
+                $('#bhxh-1').mask('9999 9999 99');
+                $('#bhxh-2').mask('9999 / 9999');
+                $('#bhxh-3').mask('B999999999999999-C999');
+                $('#date').mask('99/99/9999');
+                $('#phone').mask('9999 999 999');
+                $('#guardian-phone').mask('9999 999 999');
+                $('#phoneExt').mask('(999) 999-9999? x99999');
+                $('#iphone').mask('+33 999 999 999');
+                $('#tin').mask('99-9999999');
+                $('#ccn').mask('9999 9999 9999 9999');
+                $('#ssn').mask('999-99-9999');
+                $('#currency').mask('999,999,999.99');
+                $('#product').mask('a*-999-a999', {
+                  placeholder: ' '
+                });
+                $('#eyescript').mask('~9.99 ~9.99 999');
+                $('#po').mask('PO: aaa-999-***');
+                $('#pct').mask('99%');
+                $('#phoneAutoclearFalse').mask('(999) 999-9999', {
+                  autoclear: false
+                });
+                $('#phoneExtAutoclearFalse').mask('(999) 999-9999? x99999', {
+                  autoclear: false
+                });
+                $('input').blur(function () {
+                  $('#info').html('Unmasked value: ' + $(this).mask());
+                }).dblclick(function () {
+                  $(this).unmask();
+                });
+              })(jQuery);
+            }  
+            else if (filename == 'student-details.html') {
+              this.view.loadStudentView(data);
+            }
+            else if (filename == 'student-course.html') {
+              this.view.loadStudentCourseView(data);
+            }
+            else if (filename == 'edit-teaching-course.html') {
+              this.view.loadEditTeachingCourse(data);
+            }
+            
+
+          })
+          .catch((error) => {
+              console.log(error);
+          })
+          .then(() => {
+            this.view.bindSubmitEditStudent(this.handleEditStudent)
+          })
+          .then(() => {
+            this.model.bindOnDataChanged(this.handleOnDataChanged)
+          })
+          .then(() => {
+            this.view.bindLogin(this.handleLogin)
+          })
+          .then(() => {
+            this.view.bindLogout(this.handleLogout)
+          })
         })
 
         
     }
+    handleLogout() {
+      $("#logout").on("click", function(e) {
+        var fbcontroller = new FirebaseController()
+        fbcontroller.setData("current_user", {user_name: "onLogin"})
+      })
+    }
     handleOnDataChanged() {
-      // this.model.getStudentData("kien_trantrung")
+      // this.model.getStudentData(user_name)
       //   .then((data) => {
       //     this.view.LoadEditStudentView()
       //   })
@@ -286,7 +356,95 @@ class Controller {
 
       // var m = new Model();
       //If this arrow function is called in the subclass of Controller, it will get error
-      this.model.updateStudentData("kien_trantrung", saveToFirebaseObject);
+      this.model.updateStudentData(user_name, saveToFirebaseObject);
+    }
+    handleLogin() {
+
+      // var model = this.model
+      $(document).ready(function() {
+        $("#submit-button").on("click", function(e) {
+          e.preventDefault()
+          if (login_type == "") {
+            alert("Please select a login type!")
+          }
+          
+          var login = function() {
+            var password = ""
+            var model = new Model()
+            if (login_type == "student") {
+              model = model.getStudentData
+            }
+            else if (login_type == "teacher") {
+              model = model.getTeacherData
+            }
+            else if (login_type == "admin") {
+              model = model.getAdminData
+            }
+            model(username).then((data) => {
+              console.log(data)
+              console.log ("data password ", data.password)  
+              password = data.password
+            })
+            .then(() => { 
+              if (password == $('#password').val()) {
+                model = new Model()
+                model.setCurrentUser(login_type, username)
+                window.location.href = location
+              } else {
+                alert("Wrong password!")
+              }})
+            .catch((error) => {
+              // console.log(error)
+              alert("User not found!")
+            })
+          }
+          login()
+          
+          
+
+
+
+        })
+
+        var login_type = ""
+        var location = "#"
+        $('#login-type').on('change', function(e) {
+          login_type = $("input[type='radio'][name='option']:checked").val()
+          if (login_type == "student") {
+            location = "student-details.html";
+          }
+          else if (login_type == "teacher") {
+            location = "teacher-details.html";
+          }
+          else if (login_type == "admin") {
+            location = "index.html";
+          }
+          
+          
+        })
+
+        var username
+        var password
+        $('#username').on('input', function(e) {
+          username = $('#username').val()
+          console.log(username)
+        })
+        $('#password').on('input', function(e) {
+          password = $('#password').val()
+          console.log(password)
+        })
+
+
+
+
+
+      });
+
+      
+
+
+      
+
     }
     handleCourseRegistration() {
         // console.log("Course registration");
